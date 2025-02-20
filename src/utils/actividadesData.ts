c03c0b96b1cb49eb6add1149a5b90548012966e9
Omar <omardavidjm@ufps.edu.co>
2025-02-19 23:34:23 -0500

    feat: add new images and SVG assets for activities and backgrounds

--- src/utils/actividadesData.ts
+++ src/utils/actividadesData.ts
@@ -18,7 +18,7 @@ export const articles: Article[] = [
     title: "Modelos de ciclo de vida del software",
     description:
       "Los modelos de proceso prescriptivo fueron propuestos originalmente para poner orden en el caos del desarrollo de software (Pressman, 2010).",
-    imageUrl: "/imagenesActividades/ciclo_vida_software.jpeg",
+    imageUrl: "imagenesActividades/ciclo_vida_software.jpeg",
     imageAlt: "Ciclo de vida del software",
     docUrl: "/docsActividades/consulta_sdlc_1152263_1152277_1152280.pdf",
     link: "/actividades/ciclo_vida_software",
@@ -30,7 +30,7 @@ export const articles: Article[] = [
     title: "Sistemas de información",
     description:
       "Los sistemas de información permiten recoger y manejar de la mejor forma todos los datos con los que cuenta una empresa (OBS Business School, s.f.).",
-    imageUrl: "/imagenesActividades/sistema_informacion-edit.png",
+    imageUrl: "imagenesActividades/sistema_informacion-edit.png",
     imageAlt: "Sistemas de información",
     docUrl: "/docsActividades/consulta_sistemas_informacion_1152263_1152277_1152280.pdf",
     link: "/actividades/sistemas_informacion",
@@ -41,7 +41,7 @@ export const articles: Article[] = [
   {
     title: "Tendencias del desarrollo del software",
     description: "A la hora de crear una solución digital, los empresarios deben estar bien informados de hacia dónde se dirige la industria del software y elaborar una estrategia en consecuencia (Lores, 2023).",
-    imageUrl: "/imagenesActividades/tendencias_desarrollo_software.png",
+    imageUrl: "imagenesActividades/tendencias_desarrollo_software.png",
     imageAlt: "Tendencias del desarrollo del software",
     docUrl: "/docsActividades/consulta_tendencias_desarrollo_software_1152263_1152277_1152280.pdf",
     link: "/actividades/tendencias_desarrollo_software",
